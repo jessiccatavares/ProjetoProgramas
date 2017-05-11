74af485ed1c69109eac8ad3abf1481de6eaf07d8
jessiccatavares <jldst.snf@uea.edu.br>
2017-05-11 04:21:29 -0300

    Create Conta.java

---
+++ Conta.java
@@ -0,0 +1,36 @@
+package financeiro;
+
+public class Conta {
+	private int codigo;
+	private String tipo;
+	private float valor;
+	
+	public Conta(){}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	
+	
+
+}
